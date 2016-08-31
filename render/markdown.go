c97b4052217041f058b4443325a2e73310f24043
longkai <im.longkai@gmail.com>
2016-08-31 20:35:44 +0800

    Add a brand new markdown ID parser.

--- render/markdown.go
+++ render/markdown.go
@@ -3,14 +3,20 @@ package render
 import (
 	"bytes"
 	"encoding/json"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
+	"strings"
 	"time"
+
+	"github.com/longkai/xiaolongtongxue.com/config"
 )
 
 // Meta metadata for the markdown.
 type Meta struct {
+	ID           string
+	body         []byte    // TODO: avoid this field?
 	Title        string    `json:"title"`
 	Tags         []string  `json:"tags"`
 	Date         time.Time `json:"date"`
@@ -21,24 +27,50 @@ type Meta struct {
 	RenderOption int       `json:"render_option"`
 }
 
+// Markdown a rendered *md file.
+type Markdown struct {
+	Meta
+	Prev, Next string
+	Body       template.HTML
+}
+
 var parseJSON = func(in io.Reader, v interface{}) error {
 	b, err := ioutil.ReadAll(in)
 	if err != nil {
 		return err
 	}
+	if len(b) == 0 {
+		return nil
+	}
 	return json.Unmarshal(b, v)
 }
 
-func parseMd(in io.Reader) (*Meta, []byte, error) {
+func parseID(path string) string {
+	base := config.Env.ArticleRepo
+	if !strings.HasPrefix(path, base) {
+		return ""
+	}
+	var id string
+	if strings.HasSuffix(base, "/") {
+		id = path[strings.LastIndexByte(base, '/'):]
+	} else {
+		id = path[len(base):]
+	}
+	// trim last segment
+	return id[:strings.LastIndexByte(id, '/')+1]
+}
+
+func parseMd(in io.Reader) (*Meta, error) {
 	title, body, _json, err := parse(in)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	m := new(Meta)
 	m.Title = title
+	m.body = body
 	if err = parseJSON(bytes.NewReader(_json), m); err != nil {
 		log.Print(err)
 	}
-	return m, body, nil
+	return m, nil
 }
--- render/markdown_test.go
+++ render/markdown_test.go
@@ -1,68 +1,35 @@
 package render
 
 import (
-	"fmt"
-	"strings"
 	"testing"
+
+	"github.com/longkai/xiaolongtongxue.com/config"
 )
 
-func TestMetadataRegexp(t *testing.T) {
-	s := fmt.Sprintf("### EOF \n%sjson\n%s\n%s", fenced_block, `{
-	"key": "value"
-}`, fenced_block)
-	if !metaRegexp.MatchString(s) {
-		t.Errorf("%s not match json block!\n", s)
-	}
-	ss := metaRegexp.FindStringSubmatch(s)
-	fmt.Println(ss[1])
-}
-
-func TestTitleRegexp(t *testing.T) {
-	s := "Hello, World \n====\n"
-	if !titleRegexp.MatchString(s) {
-		t.Errorf("%s not match title block!\n", s)
-	}
-}
-
-func TestParseTitle(t *testing.T) {
-	want := "Hello, Wrold"
-	s := want + " \n===\n\n"
-	fmt.Println(s)
-	title, b := parseTitle([]byte(s))
-	if title != want {
-		t.Errorf("want %s, but got %s\n", want, title)
-	}
-
-	if len(b) != 0 {
-		t.Errorf("result should be 0, got %d \n", len(b))
-	}
-}
-
-func TestSeparateMetaAndText(t *testing.T) {
-	s := fmt.Sprintf("heheh### EOF \n%sjson\n%s\n%s", fenced_block, `{
-		"date": "2006-01-02T15:04:05+07:00"
-}`, fenced_block)
-	text, _ := separateTextAndMeta([]byte(s))
-	if strings.Contains(text, "EOF") {
-		t.Errorf("should not contain **EOF**, got %s\n", text)
-	}
-	fmt.Println(text)
-}
-
-func TestTrimLastSegment(t *testing.T) {
+func TestParseID(t *testing.T) {
+	saved := config.Env
+	defer func() { config.Env = saved }()
+	// stub
+	base := "/path/to/repo"
+	config.Env = &config.Configuration{ArticleRepo: base}
 	tests := []struct {
-		input string
-		want  string
+		input, want string
 	}{
-		{"a/a.md", "a"},
-		{"b/a.c.md", "b"},
-		{"abc.md", "abc.md"},
-		{".", "."},
+		{base + "/a/b/c.md", "/a/b/"},
+		{base + "/a", "/"},
+		{base + "/", "/"},
+		{"balabala/bala/", ""},
 	}
 
-	for _, test := range tests {
-		if got := trimLastSegment(test.input); got != test.want {
-			t.Errorf("trimLastSegment(%q) = %q, want %q\n", test.input, got, test.want)
+	f := func(t *testing.T) {
+		for _, test := range tests {
+			if got := parseID(test.input); got != test.want {
+				t.Errorf("parseID(%q) = %q, want %q\n", test.input, got, test.want)
+			}
 		}
 	}
+
+	t.Run("WithoutSuffix", f)
+	config.Env.ArticleRepo += "/"
+	t.Run("WithSuffix", f)
 }
