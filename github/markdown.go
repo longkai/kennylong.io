bafd060fc1efdce98661cd5d770cbef95bfca814
longkai <im.longkai@gmail.com>
2016-09-01 10:46:10 +0800

    Testify Github Markdown API.

--- github/markdown.go
+++ github/markdown.go
@@ -1,57 +1,41 @@
 package github
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"github.com/longkai/xiaolongtongxue.com/env"
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/longkai/xiaolongtongxue.com/config"
 )
 
 const (
 	endpoint = "https://api.github.com"
 )
 
-type markdownRender struct {
-	Text    string `json:"text"`
-	Mode    string `json:"mode"`
-	Context string `json:"context"`
-
-	reader io.Reader
+var provideURL = func(path string) string {
+	return endpoint + path
 }
 
-func (m *markdownRender) Read(p []byte) (n int, err error) {
-	if m.reader == nil {
-		bs, err := json.Marshal(m)
-		if err != nil {
-			return 0, err
-		}
-		m.reader = bytes.NewReader(bs)
-	}
-	return m.reader.Read(p)
+var provideToken = func() string {
+	return fmt.Sprintf("token %s", config.Env.AccessToken)
 }
 
-// Render the makrdown to html []byte with Github API.
-func Markdown(text string) ([]byte, error) {
-	m := &markdownRender{text, "markdown", "", nil}
-	req, err := http.NewRequest(http.MethodPost, endpoint+"/markdown", m)
+// Markdown makrdownify plain text to html with Github API.
+func Markdown(in io.Reader) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodPost, provideURL("/markdown/raw"), in)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", fmt.Sprintf("token %s", env.Config().AccessToken))
-
+	req.Header.Add("Content-Type", "text/plain")
+	req.Header.Add("Authorization", provideToken())
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	// the bytes may http error content
-	result, err := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		return result, fmt.Errorf("http StatusCode %d", resp.StatusCode)
+		return nil, fmt.Errorf("Markdown API StatusCode %d", resp.StatusCode)
 	}
-	return result, err
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
