5e8576a3eadd41185955976541c95fa4f128d017
longkai <im.longkai@gmail.com>
2016-09-02 11:34:52 +0800

    Git diff is done.

--- git/git.go
+++ git/git.go
@@ -4,79 +4,56 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os/exec"
-	"strings"
+	"regexp"
 )
 
 const (
-	Renamed   = 'R'
-	Deleted   = 'D'
-	Modified  = 'M'
-	Untracked = '?'
-	// not interested other status
-
-	spiltor = " -> "
+	add = `A`
+	mod = `M`
+	del = `D`
 )
 
-func ParseRename(s string) (string, string) {
-	files := strings.Split(s, spiltor)
-	if len(files) == 1 {
-		return files[0], ""
-	}
-	return files[0], files[1]
-}
+var (
+	re = regexp.MustCompile(`^([AMD])\s+(.*)`)
+)
 
-func Status(path string) (map[byte][]string, error) {
-	script := fmt.Sprintf("cd %s; git status --porcelain", path)
-	log.Printf("exec shell script, %q\n", script)
-	cmd := exec.Command("/bin/bash", "-c", script)
+// Diff `git diff --name-status HEAD~ HEAD`
+func Diff(repo string) (adds, mods, dels []string, err error) {
+	script := fmt.Sprintf("cd %s && git diff --name-status HEAD~ HEAD", repo)
+	log.Printf("run shell script: %q", script)
+	cmd := exec.Command("/bin/sh", "-c", script)
 	b, err := cmd.Output()
 	if err != nil {
-		return nil, err
+		return
 	}
-	fmt.Printf("%s\n", b)
-	return status(b), nil
+	adds, mods, dels = diff(bytes.NewReader(b))
+	return
 }
 
-func status(output []byte) map[byte][]string {
-	scanner := bufio.NewScanner(bytes.NewReader(output))
-	m := make(map[byte][]string)
+func diff(in io.Reader) (adds, mods, dels []string) {
+	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
-		code, path := separate(scanner.Bytes())
-		switch code {
-		case Renamed:
-			fallthrough
-		case Deleted:
-			fallthrough
-		case Modified:
-			fallthrough
-		case Untracked:
-			m[code] = append(m[code], path)
+		line := scanner.Text()
+		pair := re.FindStringSubmatch(line)
+		if len(pair) != 3 {
+			continue
+		}
+		switch pair[1] {
+		case add:
+			adds = append(adds, pair[2])
+		case mod:
+			mods = append(mods, pair[2])
+		case del:
+			dels = append(dels, pair[2])
 		default:
-			log.Printf("skip git status code %q, path is %s\n", code, path)
+			log.Printf("Unknown git diff line: %q", line)
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		log.Println(err)
+		log.Printf("parsing git diff: %v", err)
 	}
-	return m
-}
-
-// separate status code and path
-func separate(b []byte) (byte, string) {
-	if len(b) < 4 {
-		return 0, ""
-	}
-	// input may like below
-
-	// A  a.txt
-	//  M a/b/c.txt
-	// ?? path/to/file
-
-	if b[0] == ' ' {
-		return b[1], string(b[3:])
-	} else {
-		return b[0], string(b[3:])
-	}
+	return
 }
--- git/git_test.go
+++ git/git_test.go
@@ -1,128 +1,51 @@
 package git
 
 import (
-	"sort"
+	"strings"
 	"testing"
 )
 
-func TestExecStatus(t *testing.T) {
-	// just testing cmd exec successfully, don' t care the result
-	path := "."
-	_, err := Status(path)
-	if err != nil {
-		t.Errorf("Status(%q) = (_, %v), want (_, nil)\n", path, err)
+func TestDiffCmd(t *testing.T) {
+	// exe the command in the current repo is okay.
+	if _, _, _, err := Diff(`.`); err != nil {
+		t.Errorf("Diff(.): %v", err)
 	}
 }
 
-func TestStatus(t *testing.T) {
-	test := struct {
-		input []byte
-		want  map[byte][]string
+func TestDiff(t *testing.T) {
+	input := `
+A       cp2.txt
+D       cp1.txt
+M       dir/inner.txt
+D       renamef1.txt
+A       re\ namef1.txt
+D       re namef1.txt
+?       what.txt
+`
+
+	wants := struct {
+		a []string
+		m []string
+		d []string
 	}{
-		[]byte(`M  a.txt
-R  a.txt -> b.txt
- M a.txt
-D  b.txt
-?? a/b/c.txt
-A  a.txt
-`), map[byte][]string{
-			Renamed:   []string{"a.txt -> b.txt"},
-			Deleted:   []string{"b.txt"},
-			Modified:  []string{"a.txt", "a.txt"},
-			Untracked: []string{"a/b/c.txt"},
-		},
+		[]string{"cp2.txt", `re\ namef1.txt`},
+		[]string{"dir/inner.txt"},
+		[]string{"cp1.txt", "renamef1.txt", "re namef1.txt"},
 	}
 
-	if got := status(test.input); !mapEquals(got, test.want) {
-		t.Errorf("status(\"%s\") = %v, want %v\n", test.input, got, test.want)
+	if a, m, d := diff(strings.NewReader(input)); !sliceEq(a, wants.a) || !sliceEq(m, wants.m) || !sliceEq(d, wants.d) {
+		t.Errorf("diff(%s) = (%s, %s, %s), want (%s, %s, %s)", input, a, m, d, wants.a, wants.m, wants.d)
 	}
 }
 
-func mapEquals(m1, m2 map[byte][]string) bool {
-	keys1, keys2 := make([]int, 0), make([]int, 0)
-	// no generic, hehe
-	for k := range m1 {
-		keys1 = append(keys1, int(k))
-	}
-	for k := range m2 {
-		keys2 = append(keys2, int(k))
-	}
-	// test keys are equal
-	sort.Ints(keys1)
-	sort.Ints(keys2)
-	if !intsEquals(keys1, keys2) {
-		return false
-	}
-	// test values are equal
-	for _, k := range keys1 {
-		v1, v2 := m1[byte(k)], m2[byte(k)]
-		if !stringsEquals(v1, v2) {
-			return false
-		}
-	}
-	return true
-}
-
-func intsEquals(a, b []int) bool {
+func sliceEq(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i := range a {
+	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			return false
 		}
 	}
 	return true
-}
-
-func stringsEquals(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func TestSeparate(t *testing.T) {
-	tests := []struct {
-		input    []byte
-		wantCode byte
-		wantPath string
-	}{
-		{[]byte(`A  a.txt`), 'A', "a.txt"},
-		{[]byte(` M a.txt`), 'M', "a.txt"},
-		{[]byte(`?? a/b/c.txt`), '?', "a/b/c.txt"},
-		{[]byte(""), 0, ""},
-		{[]byte("A  "), 0, ""},
-		{[]byte("A   "), 'A', " "},
-	}
-
-	for _, test := range tests {
-		if code, path := separate(test.input); code != test.wantCode || path != test.wantPath {
-			t.Errorf("separate(%q) = (%q, %q), want (%q, %q)\n", test.input, code, path, test.wantCode, test.wantPath)
-		}
-	}
-}
-
-func TestFromRename(t *testing.T) {
-	tests := []struct {
-		input    string
-		wantFrom string
-		wantTo   string
-	}{
-		{"a.go -> b.go", "a.go", "b.go"},
-		{"a.go ->", "a.go ->", ""},
-		{"  -> b.go", " ", "b.go"},
-		{"a.go b.go", "a.go b.go", ""},
-	}
-
-	for _, test := range tests {
-		if gotFrom, gotTo := ParseRename(test.input); gotTo != test.wantTo || gotFrom != test.wantFrom {
-			t.Errorf("ParseRename(%q) = (%q, %q), want (%q, %q)\n", test.input, gotFrom, gotTo, test.wantFrom, test.wantTo)
-		}
-	}
 }
